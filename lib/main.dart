192b85ba36515c40405decc03b9f1e83519cefdd
unknown <shubham.tade@gmail.com>
2022-03-05 20:16:27 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    backgroundColor: Color.red,
+    appBar: AppBar(
+      title: Text("Shubham"),
+    ),
+  )));
+}
